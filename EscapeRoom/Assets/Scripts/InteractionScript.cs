3d67e161889a571d7d9ece5b92e1faf3f4baa682
Owen H <owenhowarth@gmail.com>
2019-11-15 12:45:15 +0000

    Started raycast stuff

---
+++ EscapeRoom/Assets/Scripts/InteractionScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log(
+                "e");
+            int layerMask = 1 << 8;
+            layerMask = ~layerMask;
+
+            Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+
+        }
+    }
+}
