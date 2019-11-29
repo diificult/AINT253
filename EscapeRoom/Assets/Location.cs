2b6edbe28dfbd71ec639193be41227614082547a
Owen H <owenhowarth@gmail.com>
2019-11-29 19:15:18 +0000

    Prefabed the slider tiles

--- EscapeRoom/Assets/Location.cs
+++ EscapeRoom/Assets/Location.cs
@@ -1,8 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Location : MonoBehaviour
 {
     public int x, y;
+    public int value;
+    [SerializeField]
+    private Text number;
+
+    private void OnEnable()
+    {
+        
+    }
 }
