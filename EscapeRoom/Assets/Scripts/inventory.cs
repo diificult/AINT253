bd30cf1f8f9dd82d1f76f9a3b0617cdb1b7e221d
Owen H <owenhowarth@gmail.com>
2019-12-13 12:29:25 +0000

    Roof
    Added a roof

--- EscapeRoom/Assets/Scripts/inventory.cs
+++ EscapeRoom/Assets/Scripts/inventory.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-[SerializeField]
-public class OnInvUpdate UnityEvent : <string>();
+[System.Serializable]
+public class OnInvUpdate : UnityEvent<string>{}
 
 public class inventory : MonoBehaviour
 {
 
     string handItem = "";
 
-    public 
+    public OnInvUpdate update;
 
     public void setHandItem(string item)
     {
         if (handItem == "")
         {
             handItem = item;
+            update.Invoke("Hand: " + handItem);
         }
     }
     
---
+++ EscapeRoom/Assets/updateText.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class updateText : MonoBehaviour
+{
+    public void textUpdate(string newText)
+    {
+        GetComponent<Text>().text = newText;
+    }
+}
