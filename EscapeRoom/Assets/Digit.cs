7384a87c9d8800504b4043929697ca5da24e8f3a
Owen H - Owen Home <owenhowarth@gmail.com>
2019-12-20 15:00:03 +0000

    Lock Puzzle and  Worktops
    Created the lock  puzzle! Now fully works,  next needs to me implemented  into a  cupboard.
    Created the worktop model  and has  been textured

---
+++ EscapeRoom/Assets/Digit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Digit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void digitUp()
+    {
+        int value = int.Parse(GetComponent<Text>().text);
+        if (value == 9) value = 0;
+        else value++;
+        GetComponent<Text>().text = ""+value;
+        
+    }
+    public void digitDown()
+    {
+        int value = int.Parse(GetComponent<Text>().text);
+        if (value == 0) value = 9;
+        else value--;
+        GetComponent<Text>().text = "" + value;
+
+    }
+}
---
+++ EscapeRoom/Assets/LockPuzzle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LockPuzzle : MonoBehaviour
+{
+
+    public GameObject[] digits = new GameObject[4];
+    string solution = "2976";
+    public void CheckCode()
+    {
+        string usercode = "";
+        foreach (GameObject digit in digits)
+        {
+            usercode += digit.GetComponent<Text>().text;
+        }
+        if (usercode == solution)
+        {
+            Debug.Log("Solution Found");
+        }
+
+    }
+}
